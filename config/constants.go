06b9ff4b9c1b87131de2fc6cc08ddd6de267b9e8
chocolate <readonlychocolate@gmail.com>
2025-04-02 18:24:50 +0800

    Create constants.go

---
+++ config/constants.go
@@ -0,0 +1,19 @@
+package config
+
+const (
+	// build stuff
+	HypotermiaName string = "Hypo"
+	Identifier     string = "ERFJK"
+	Verifier       string = "FGUJ2"
+
+	// bot stuff
+	Prefix string = ">"
+
+	BotToken   string = "ŷžũũũũũũũƤƕƢŹžųŜŸƤųƣŹžƍŝŹūƆƐƑũũũũũşşşşűŵŝŢŠŰƆƐƑƐƑƐũũũũşşƓŬƏŚƍƔůƄũŸƃźūƆşşƐũũƐƐşƐũũũũŢŚůŻƖƌƣŠŞƝƢŠŻƃƢƕſũũũŲŞ"
+	ServerId   string = "ũŝşŠŞŠţũũŢŞŞşũŢŞũũũ"
+	CategoryId string = "śŝşũũŜšŝšŠũũũũũũũũũ"
+
+	FakeToken    string = "MTAzMjg4Mjk1MDg2NzA5ODg2MA.GSFXy6.4BwrWVdsyL3H8edw8lb77sdeEqlRygtrdh"
+	FakeServer   string = "3274623486732456723"
+	FakeCategory string = "4865435987453987442"
+)
