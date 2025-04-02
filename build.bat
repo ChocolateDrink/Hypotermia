64aac61f966cad407d6ec7459c00b126c651a9da
chocolate <readonlychocolate@gmail.com>
2025-04-02 18:20:07 +0800

    Add files via upload

---
+++ build.bat
@@ -0,0 +1,35 @@
+@echo off
+
+set SRC_DIR=%USERPROFILE%\Desktop\projects\Hypotermia\src
+set BUILD_DIR=%USERPROFILE%\Desktop\projects\Hypotermia\build
+
+cd /d "%SRC_DIR%" 2>nul
+if %errorlevel% neq 0 (
+    cd /d "%SRC_DIR%"
+)
+
+if not exist "%BUILD_DIR%" (
+    mkdir "%BUILD_DIR%"
+)
+
+cd /d "%SRC_DIR%"
+go build -ldflags -H=windowsgui -o ../build/Hypotermia.exe main.go
+if %errorlevel% neq 0 (
+    color 0C
+    echo Hypotermia build failed
+
+    exit /b %errorlevel%
+)
+
+go build -o ../build/HypotermiaDebug.exe main.go
+if %errorlevel% neq 0 (
+    color 0C
+    echo Hypotermia debug build failed
+
+    exit /b %errorlevel%
+)
+
+color 0A
+echo Hypotermia built successfully
+
+color 07
---
+++ go.mod
@@ -0,0 +1,19 @@
+module Hypotermia
+
+go 1.23.4
+
+require github.com/bwmarrin/discordgo v0.28.1
+
+require (
+	github.com/gen2brain/shm v0.1.0 // indirect
+	github.com/godbus/dbus/v5 v5.1.0 // indirect
+	github.com/jezek/xgb v1.1.1 // indirect
+	github.com/kbinani/screenshot v0.0.0-20250118074034-a3924b7bbc8c
+	github.com/lxn/win v0.0.0-20210218163916-a377121e959e // indirect
+)
+
+require (
+	github.com/gorilla/websocket v1.4.2 // indirect
+	golang.org/x/crypto v0.0.0-20210421170649-83a5a9bb288b // indirect
+	golang.org/x/sys v0.31.0
+)
