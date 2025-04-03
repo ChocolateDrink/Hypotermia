d8661b25d88e99509940f19d81ea85ca163aa998
chocolate <readonlychocolate@gmail.com>
2025-04-03 12:39:58 +0800

    anti vm config

--- config/constants.go
+++ config/constants.go
@@ -16,4 +16,7 @@ const (
 	FakeToken    string = "MTAzMjg4Mjk1MDg2NzA5ODg2MA.GSFXy6.4BwrWVdsyL3H8edw8lb77sdeEqlRygtrdh"
 	FakeServer   string = "3274623486732456723"
 	FakeCategory string = "4865435987453987442"
+
+	// misc
+	AntiVM bool = false
 )
