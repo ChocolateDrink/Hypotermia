d50f420be1749e94c45d693333c2bca1cf36bd4d
chocolate <readonlychocolate@gmail.com>
2025-04-25 19:59:46 +0800

    Update constants.go

--- config/constants.go
+++ config/constants.go
@@ -24,9 +24,9 @@ const (
 	ServerId   string = ""
 	CategoryId string = ""
 
-	FakeToken    string = "MTAzMjg4Mjk1MDg2NzA5ODg2MA.GSFXy6.4BwrWVdsyL3H8edw8lb77sdeEulRygtrdh"
-	FakeServer   string = "3274623486732456723"
-	FakeCategory string = "4865435987453987442"
+	FakeToken    string = " MTAzMjg4Mjk1MDg2NzA5ODg2MA.GSFXy6.4BwrWVdsyL3H8edw8lb77sdeEulRygtrdh "
+	FakeServer   string = " 3274623486732456723 "
+	FakeCategory string = " 4865435987453987442 "
 
 	// misc
 	AntiVM bool = false
