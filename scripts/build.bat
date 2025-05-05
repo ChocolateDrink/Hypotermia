e6a14f9a3c9edf414c79ffdb32edbb3a3481bee9
chocolate <readonlychocolate@gmail.com>
2025-05-05 13:19:49 +0800

    use tabs

--- scripts/build.bat
+++ scripts/build.bat
@@ -5,21 +5,21 @@ set BUILD_DIR=%USERPROFILE%\Desktop\projects\Hypothermia\build
 
 cd /d "%SRC_DIR%" 2>nul
 if %errorlevel% neq 0 (
-    cd /d "%SRC_DIR%"
+	cd /d "%SRC_DIR%"
 )
 
 if not exist "%BUILD_DIR%" (
-    mkdir "%BUILD_DIR%"
+	mkdir "%BUILD_DIR%"
 )
 
 cd /d "%SRC_DIR%"
 
 go build -trimpath -ldflags="-w -s -H=windowsgui" -o "%BUILD_DIR%\Hypothermia.exe" main.go
 if %errorlevel% neq 0 (
-    color 0C
-    echo Hypothermia build failed
+	color 0C
+	echo Hypothermia build failed
 
-    exit /b %errorlevel%
+	exit /b %errorlevel%
 )
 
 color 0A
--- scripts/dropper.bat
+++ scripts/dropper.bat
@@ -4,17 +4,17 @@ set HELPERS_DIR=%USERPROFILE%\Desktop\projects\Hypothermia\_helpers
 set BUILD_DIR=%USERPROFILE%\Desktop\projects\Hypothermia\build
 
 if not exist "%BUILD_DIR%" (
-    mkdir "%BUILD_DIR%"
+	mkdir "%BUILD_DIR%"
 )
 
 cd /d "%HELPERS_DIR%"
 
 go build -trimpath -ldflags="-w -s" -o "%BUILD_DIR%\Dropper.exe" dropper.go
 if %errorlevel% neq 0 (
-    color 0C
-    echo Dropper build failed
+	color 0C
+	echo Dropper build failed
 
-    exit /b %errorlevel%
+	exit /b %errorlevel%
 )
 
 color 0A
